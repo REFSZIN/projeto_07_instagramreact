37131e33cf70ce276fabe492afce48f698877425
REFSZIN <refwire@gmail.com>
2022-05-08 19:47:22 -0300

    Fix Borda

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Background from '../data/story-circle.svg';
-const borda = {
-backgroundImage: "url(" + { Background } + ")"
-};
+
 export default function Story (props) {
     return (
         <article class="story">
-            <figure style={ borda } class="imagem">
+            <figure id="borda" class="imagem">
                 <img src={props.img} alt="img"/>
             </figure>
             <figcaption class="usuario">{props.name}</figcaption>
