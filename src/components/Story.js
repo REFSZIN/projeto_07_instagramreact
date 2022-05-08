e94db4a2dfeaddcc2eae29b35e929888668db312
REFSZIN <refwire@gmail.com>
2022-05-08 20:05:15 -0300

    Bord Circle

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Story (props) {
     return (
         <article class="story">
-            <figure id="borda" class="imagem">
+            <figure class="imagem">
                 <img src={props.img} alt="img"/>
             </figure>
             <figcaption class="usuario">{props.name}</figcaption>
